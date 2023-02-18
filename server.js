43de6580afd9d0928b0041ddb24ee071f783ffc6
TaronKaragyan <taron.karagyan.y@tumo.org>
2023-02-18 12:48:30 +0400

    4

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(my_modules));
+app.use(express.static("my_modules"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-res.redirect(index.html);
+    res.redirect("index.html");
 
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
 
-console.log("Example is running on port 3000");
+    console.log("Example is running on port 3000");
 
 });
